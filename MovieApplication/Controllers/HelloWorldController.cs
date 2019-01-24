90d53add6531d89e7cad95f72acfae361233b51e
kavyamekala93 <kavyamekala9@gmail.com>
2019-01-23 22:16:56 -0500

    Commit

---
+++ MovieApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Welcome(string name, int numTimes = 5)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.numTimes = numTimes;
+            return View();
+        }
+        /*public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }*/
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        /*public string Welcome(string name, int numTimes = 1)
+        {
+            //return System.Web.HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+            return View(); 
+        }*/
+    }
+}
---
+++ MovieApplication/Views/HelloWorld/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieApplication.Views.HelloWorld
+{
+    public class Movie
+    {
+    }
+}
